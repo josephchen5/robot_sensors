8e5cd6b6d9609dee177ce066804053bb540ccbc5
josephchen5 <joseph.chen4210@gmail.com>
2018-03-01 23:56:25 +0800

    rules

--- rules/initenv.sh
+++ rules/initenv.sh
@@ -3,8 +3,8 @@ echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MOD
 echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a8", MODE:="0666", GROUP:="dialout",  SYMLINK+="ublox_gps"' >/etc/udev/rules.d/UBlox_GPS.rules
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0666", GROUP:="dialout",  SYMLINK+="razor_imu_10736"' >/etc/udev/rules.d/razor_imu_10736.rules
 echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="26ac", ATTRS{idProduct}=="0011", MODE:="0666", GROUP:="dialout",  SYMLINK+="px4"' >/etc/udev/rules.d/px4.rules
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0666", GROUP:="dialout",  SYMLINK+="nano"' >/etc/udev/rules.d/arduino_NANO.rules
 
 
-service udev reload
 sleep 2
 service udev restart
